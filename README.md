84c11e27e087894facf70ad3ee2af030e805c2d5
Lee <leesamso@88e9fe68e5cf.ant.amazon.com>
2019-09-20 09:46:44 +0800

    added html-javascript site for comparison

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Lalala
+# cuteApp
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.0.
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, ViewChild, ElementRef} from '@angular/core';
+import * as AWS from "aws-sdk";
+import { PromiseResult } from "aws-sdk/lib/request";
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
@@ -11,6 +13,18 @@ import { Injectable } from '@angular/core';
 export class AppComponent {
   title = 'lalala';
 
+  public rekognition: AWS.Rekognition;
+
+  constructor(){
+        AWS.config.region = 'ap-southeast-1';
+        AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+            IdentityPoolId: 'ap-southeast-1:5d8ba300-588e-4c78-9d0a-b1dbf064a381'
+        });
+        //AWS.config.getCredentials;  
+        //AWS.config.loadFromPath('/../credentials.json');
+        this.rekognition = new AWS.Rekognition();
+    }
+
   @ViewChild("video", {static:false})
     public video: ElementRef;
 
@@ -19,15 +33,16 @@ export class AppComponent {
 
     public captures: Array<any>;
 
-    public dataURL: Array<any>;
+    public dataURI: string;
+
+    public imageBlob: string;
+
+    public identifiedPerson: string;
 
     public url: Array<any>;
 
-    public constructor() {
-        this.captures = [];
-    }
-
     public ngOnInit() { }
+
 //Start Camera 
     public ngAfterViewInit() {
         if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
@@ -41,9 +56,55 @@ export class AppComponent {
 //Capture Image 
     public capture() {
         var context = this.canvas.nativeElement.getContext("2d").drawImage(this.video.nativeElement, 0, 0, 640, 480);
+        
+        this.captures = [];
         this.captures.push(this.canvas.nativeElement.toDataURL("data:image/png;base64"));
-        var dataURL = this.canvas.nativeElement.toDataURL("data:image/png;base64").value;
+        console.log(this.captures);
 
-      }
+        //this.dataURI = this.canvas.nativeElement.toDataURL("data:image/png;base64").value;
+        this.dataURI = this.canvas.nativeElement.toDataURL("data:image/png;base64,");
+
+        this.dataURLtoBlob(this.dataURI);
+
+        //const imageFile = new File([imageBlob], 'cameraImage.jpeg', { type: 'image/jpeg' });
+    }
+
+//Convert dataURL to imageBytes
+    public dataURLtoBlob(dataURI) {
+        console.log(dataURI);
+        //const byteString = window.atob(dataURI);
+        const byteString = atob(dataURI.split("data:image/png;base64,")[1]);
+        console.log(byteString);
+        console.log(byteString.length);
+
+        const imageBytes = new ArrayBuffer(byteString.length);
+
+        this.SearchFace(imageBytes).then(
+            (data) => {
+                console.log(data.FaceMatches[0].Face.ExternalImageId, ' had been identified');
+                this.identifiedPerson = data.FaceMatches[0].Face.ExternalImageId;
+        }).catch((err) => {
+            console.error(err);
+        });
+
+        return this.identifiedPerson;
+     }
+    
+
+//Call Rekognition API for Face Searching
+    public SearchFace(imageBytes):
+        //Promise<PromiseResult<AWS.Rekognition.SearchFacesByImageResponse, AWS.AWSError>> {
+        Promise<PromiseResult<AWS.Rekognition.SearchFacesByImageResponse, AWS.AWSError>> {
+            console.log(imageBytes);
+            
+            var params = {
+                CollectionId: 'cx-demo-rekognition',
+                Image: {
+                    Bytes: imageBytes
+              }
+            };
+
+            return this.rekognition.searchFacesByImage(params).promise();
+        }
 }
 
