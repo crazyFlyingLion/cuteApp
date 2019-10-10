4c4c4fe03b4957ad121263dcdbfe13b10428cde7
Lee <leesamso@88e9fe68e5cf.ant.amazon.com>
2019-10-11 00:21:30 +0800

    added voice capturing module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,5 @@
+declare var MediaRecorder: any;
+
 import { Component, ViewChild, ElementRef} from '@angular/core';
 import * as AWS from "aws-sdk";
 import * as Textract from 'aws-sdk/clients/textract';
@@ -12,35 +14,142 @@ import { PromiseResult } from "aws-sdk/lib/request";
 export class AppComponent {
   title = 'cuteApp';
 
-  constructor(){
-        AWS.config.region = 'us-east-1'; // Region
-        AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-            IdentityPoolId: 'us-east-1:a30e0d17-0ec6-4dac-8fba-de2695d2e738',
-        });
-    }
+//   constructor(){
+//         AWS.config.region = 'us-east-1'; // Region
+//         AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+//             IdentityPoolId: 'us-east-1:a30e0d17-0ec6-4dac-8fba-de2695d2e738',
+//         });
+//     }
 
-  @ViewChild("video", {static:false})
+    @ViewChild("video", {static:false})
     public video: ElementRef;
+
+    @ViewChild("audio", {static:false})
+    public audio: ElementRef;
 
     @ViewChild("canvas", {static:false})
     public canvas: ElementRef;
 
+    @ViewChild("bookingReference", {static:false})
+    public bookingReference:ElementRef;
+
     public captures: Array<any>;
 
-    public ngOnInit() { }
+    public flightStatus: Promise<string>;
 
-    //Start Camera 
+    public stream: any;
+
+    chatMessages: Array<Messages> = [{
+        Owner: 'ChatBot', 
+        Message: 'Hello, I can help you to query tickets.'}];
+    sessionAttributes = {};
+    lexRuntime: AWS.LexRuntime;
+    lexUserId = 'userID' + Date.now(); // Client application userID
+    ChatInput = '';
+
+    public ngOnInit() { 
+        AWS.config.region = 'us-east-1'; // Region
+        AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+            IdentityPoolId: 'us-east-1:a30e0d17-0ec6-4dac-8fba-de2695d2e738'
+        });
+    }
+
     public ngAfterViewInit() {
+    }
+
+    public capture_voice(){
         if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+            navigator.mediaDevices.getUserMedia({ audio: true })
+                .then(stream => {
+                    const cuteMediaRecorder = new MediaRecorder(stream);
+                    cuteMediaRecorder.start();
+
+                    var audioChunks = [];
+                    var audioLength = 0;
+
+                    cuteMediaRecorder.addEventListener("dataavailable", event => {
+                        audioChunks.push(event.data);
+                        audioLength += audioChunks.length;
+                    });
+                
+                    cuteMediaRecorder.addEventListener("stop", () => {
+                        
+                        console.log(audioChunks);
+                        const audioBlob = new Blob(audioChunks, {type: 'audio/x-l16; sample-rate=16000'});
+                        this.chat(audioBlob);
+                        
+                        //const audioUrl = URL.createObjectURL(audioBlob);
+                        //const audio = new Audio(audioUrl);
+                        //audio.play();
+                    });
+                
+                    setTimeout(() => {
+                      cuteMediaRecorder.stop();
+                    }, 3000);
+            });
+        }
+    }
+
+    public chat(audio){
+
+        this.lexRuntime = new AWS.LexRuntime();
+
+        //if (data1.chatInput !== '') {
+            //this.chatMessages.push({Owner: 'User', Message: data1.chatInput});
+            this.ChatInput = '';
+            const params = {
+                botAlias: 'beta', // your bot's alias
+                botName: 'SearchFlight', // your chatbot name 
+                contentType: 'audio/x-l16; sample-rate=16000',
+                userId: this.lexUserId,
+                //inputText: "may i search for flight",
+                accept: 'audio/mpeg',
+                inputStream: audio
+            };
+
+            console.log(params);
+
+            this.lexRuntime.postContent(params, (err, data) => {
+                if (err) {
+                   console.log(err, err.stack);
+                }
+                if (data) {
+                   //this.sessionAttributes = data.sessionAttributes;
+                   //this.chatMessages.push({Owner: 'Chatbot', Message: data.message});
+                   console.log(data);
+                   //console.log(data.message);
+                }
+            });
+         //}
+    }
+
+    public change_cam_mode(){
+        if( document.getElementById("video").hidden){
+            if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+                navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
+                    this.video.nativeElement.srcObject = stream;
+                    this.video.nativeElement.play();
+                });
+            }
+            document.getElementById("video").hidden = false;
+        }else{
+            document.getElementById("video").hidden = true;
             navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
-                this.video.nativeElement.srcObject = stream;
-                this.video.nativeElement.play();
+                var tracks = stream.getTracks();
+
+                tracks.forEach(function(track) {
+                    track.stop();
+                  });
+
+                this.video.nativeElement.srcObject = null;
+                this.video.nativeElement.pause();
             });
         }
     }
 
     //Capture Image 
-    public capture_face() {
+    public async capture_face() {
+
         var context = this.canvas.nativeElement.getContext("2d").drawImage(this.video.nativeElement, 0, 0, 640, 480);
 
         this.captures = [];
@@ -55,43 +164,21 @@ export class AppComponent {
 
         this.SearchFace(this.dataURItoBlob(dataURI)).then(
             (data) => {
-                element.innerHTML = data.FaceMatches[0].Face.ExternalImageId.replace('_', ' ').toUpperCase() + ' had been identified';
+                //element.innerHTML = data.FaceMatches[0].Face.ExternalImageId.replace('_', ' ').toUpperCase() + ' had been identified';
+                element.innerHTML = this.formatName(data.FaceMatches[0].Face.ExternalImageId) + ' had been identified';
                 console.log(data.FaceMatches[0].Face.ExternalImageId, ' had been identified');
-        }).catch((err) => {
-            console.error(err);
-        });
+                console.log('Given Name: ' + this.formatName(data.FaceMatches[0].Face.ExternalImageId).split(' ')[0] + ' || Surename: ' + this.formatName(data.FaceMatches[0].Face.ExternalImageId).split(' ')[1]);
+                
+                this.flightStatus = this.retrieveBooking(this.bookingReference.nativeElement.value, this.formatName(data.FaceMatches[0].Face.ExternalImageId).split(' ')[1], this.formatName(data.FaceMatches[0].Face.ExternalImageId).split(' ')[0]);
+                //document.getElementById('status').innerHTML = this.flightStatus.toString();
+                //console.log(status);
 
-    }
+                // var flightDetails = bookingDetails.journeys[0].passengers[0].flights[0];
+                // console.log(flightDetails);
 
-    public capture_passport() {
-        var context = this.canvas.nativeElement.getContext("2d").drawImage(this.video.nativeElement, 0, 0, 640, 480);
+                // document.getElementById('status').innerHTML = flightDetails.operateCompany + flightDetails.operateFlightNumber + " is your flight upcoming flight. " + "This flight will be departure from " + flightDetails.originPort + " to " + flightDetails.destPort + " at " + flightDetails.depatureTime + ". ";
 
-        this.captures = [];
-        this.captures.push(this.canvas.nativeElement.toDataURL("data:image/png;base64"));
-        //console.log(this.captures);
-
-        var dataURI = this.canvas.nativeElement.toDataURL("data:image/png;base64");
-        //console.log(dataURI);
-
-        var element = document.getElementById("msg");
-        element.innerHTML = "Processing Image...";
-
-        this.ScanPassport(this.dataURItoBlob(dataURI)).then(
-            (data) => {
-                var scannedText = "";
-                for (var i = 0; i < data.Blocks.length; i++) {
-                        if (i !== (data.Blocks.length - 1)) {
-                            if (i !== 0){
-                                scannedText += "[" + i + "] " + data.Blocks[i].Text + ", ";
-                            }
-                        }
-                        else {
-                            scannedText += "[" + i + "] " + data.Blocks[i].Text
-                        }
-                }
-                element.innerHTML = "Scanned Text: " + scannedText;
-                console.log(data);
-        }).catch((err) => {
+            }).catch((err) => {
             console.error(err);
         });
 
@@ -128,20 +215,120 @@ export class AppComponent {
 
         }
 
-    public ScanPassport(imageBytes):
-        Promise<PromiseResult<AWS.Textract.DetectDocumentTextResponse, AWS.AWSError>> {
+    public formatName(string) {
+        return string.split('_')[0].charAt(0).toUpperCase() + string.split('_')[0].slice(1) + " " + string.split('_')[1].charAt(0).toUpperCase() + string.split('_')[1].slice(1);
+    }
 
-            var params = {
-                Document: {
-                    Bytes: imageBytes
-                }
-            };
+    public async retrieveBooking(rloc, familyName, givenName){
+        console.log(rloc, familyName, givenName);
 
-            const textract = new Textract();
+        var url = "https://t0.api.osc1.ct1.cathaypacific.com/hackathon-apigw/api/v1/olci/getBooking?" + "rloc=" + rloc + "&familyName=" + familyName + "&givenName=" + givenName;
+        console.log(url);
 
-            return textract.detectDocumentText(params).promise();
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'apikey': 'mP7WtIx8eiscnk3aHnHAYC3voaWsWxx2'
+            }
+        });
 
-        }
-    
+        var result = await response.json();
+
+        console.log(result);
+
+        var flightDetails = result.journeys[0].passengers[0].flights[0];
+
+        var status = flightDetails.operateCompany + flightDetails.operateFlightNumber + " is your upcoming flight. " + "This flight will be departure from " + flightDetails.originPort + " to " + flightDetails.destPort + " at " + flightDetails.departureTime + ". ";
+
+        document.getElementById('status').innerHTML = status;
+
+        return status;
+
+    }
+
+    //Post the check in api request
+    public async check_in(rloc, familyName, givenName){
+        console.log(rloc, familyName, givenName);
+
+        var url = "https://t0.api.osc1.ct1.cathaypacific.com/hackathon-apigw/api/v1/olci/getBooking?" + "rloc=" + rloc + "&familyName=" + familyName + "&givenName=" + givenName;
+        console.log(url);
+
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'apikey': 'mP7WtIx8eiscnk3aHnHAYC3voaWsWxx2'
+            }
+        });
+
+        var result = await response.json();
+
+        console.log(result);
+
+        var flightDetails = result.journeys[0].passengers[0].flights[0];
+
+        var status = flightDetails.operateCompany + flightDetails.operateFlightNumber + " is your upcoming flight. " + "This flight will be departure from " + flightDetails.originPort + " to " + flightDetails.destPort + " at " + flightDetails.departureTime + ". ";
+
+        document.getElementById('status').innerHTML = status;
+
+        return status;
+
+    }
 }
+
+export interface Messages {
+    Owner:string;
+    Message:string;
+}
+
+
+
+//  // Unused Code
+//  // public capture_passport() {
+    //     var context = this.canvas.nativeElement.getContext("2d").drawImage(this.video.nativeElement, 0, 0, 640, 480);
+
+    //     this.captures = [];
+    //     this.captures.push(this.canvas.nativeElement.toDataURL("data:image/png;base64"));
+    //     //console.log(this.captures);
+
+    //     var dataURI = this.canvas.nativeElement.toDataURL("data:image/png;base64");
+    //     //console.log(dataURI);
+
+    //     var element = document.getElementById("msg");
+    //     element.innerHTML = "Processing Image...";
+
+    //     this.ScanPassport(this.dataURItoBlob(dataURI)).then(
+    //         (data) => {
+    //             var scannedText = "";
+    //             for (var i = 0; i < data.Blocks.length; i++) {
+    //                     if (i !== (data.Blocks.length - 1)) {
+    //                         if (i !== 0){
+    //                             scannedText += "[" + i + "] " + data.Blocks[i].Text + ", ";
+    //                         }
+    //                     }
+    //                     else {
+    //                         scannedText += "[" + i + "] " + data.Blocks[i].Text
+    //                     }
+    //             }
+    //             element.innerHTML = "Scanned Text: " + scannedText;
+    //             console.log(data);
+    //     }).catch((err) => {
+    //         console.error(err);
+    //     });
+
+    // }
+
+    // public ScanPassport(imageBytes):
+    //     Promise<PromiseResult<AWS.Textract.DetectDocumentTextResponse, AWS.AWSError>> {
+
+    //         var params = {
+    //             Document: {
+    //                 Bytes: imageBytes
+    //             }
+    //         };
+
+    //         const textract = new Textract();
+
+    //         return textract.detectDocumentText(params).promise();
+
+    //     }
 
